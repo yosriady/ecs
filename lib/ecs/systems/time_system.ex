3a95e7fa4ac4ec130a2817e7a006f1b1c80f20c8
Yos Riady <yosriady@gmail.com>
2016-09-16 10:58:18 +0800

    Set methods to private

--- lib/ecs/systems/time_system.ex
+++ lib/ecs/systems/time_system.ex
@@ -3,17 +3,13 @@ defmodule TimeSystem do
     Increments ages of TimeComponents
   """
 
-  def components do
-    ECS.Registry.get(:"Elixir.TimeComponent")
-  end
-
   def process do
     components()
       |> Enum.each(fn (pid) -> dispatch(pid, :increment) end)
   end
 
   # dispatch() is a pure reducer that takes in a state and an action and returns a new state
-  def dispatch(pid, action) do
+  defp dispatch(pid, action) do
     %{id: _pid, state: state} = ECS.Component.get(pid)
 
     new_state = case action do
@@ -27,5 +23,9 @@ defmodule TimeSystem do
 
     IO.puts("Updated #{inspect pid} to #{inspect new_state}")
     ECS.Component.update(pid, new_state)
+  end
+
+  defp components do
+    ECS.Registry.get(:"Elixir.TimeComponent")
   end
 end
