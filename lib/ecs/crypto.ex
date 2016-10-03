8348bebd23c1d0073f98d60c141d582dc721474d
Yos Riady <yosriady@gmail.com>
2016-10-03 17:59:01 +0800

    Update crypto.ex

--- lib/ecs/crypto.ex
+++ lib/ecs/crypto.ex
@@ -1,9 +1,12 @@
 defmodule ECS.Crypto do
+  @moduledoc """
+    Utility function(s) for entity ID generation
+  """
 
+  @doc "Generates a random base64 string of specified length"
   def random_string(length) do
     :crypto.strong_rand_bytes(length)
       |> Base.url_encode64
       |> binary_part(0, length)
   end
-
 end
