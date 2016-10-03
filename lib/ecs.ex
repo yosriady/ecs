ef92da767f7a8700a45bc3ffe9eb7f6945dbdb17
Yos Riady <yosriady@gmail.com>
2016-10-03 18:01:55 +0800

    Update ecs.ex

--- lib/ecs.ex
+++ lib/ecs.ex
@@ -1,7 +1,9 @@
 defmodule ECS do
   @moduledoc """
 
-    What's updog?
+    Entity-Component-System (ECS) is a distributed and compositional architectural design pattern 
+    that is mostly used in game development. It enables flexible decoupling of domain-specific behaviour, 
+    which overcomes some of the drawbacks of traditional object-oriented inheritance.
 
   """
 end
