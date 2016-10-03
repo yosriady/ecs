d9acd9a53f8ff7c50b5c4e078c3c14fed4e70eed
Yos Riady <yosriady@gmail.com>
2016-10-03 17:54:44 +0800

    Delete processes.ex

--- lib/ecs/processes.ex
+++
@@ -1,7 +0,0 @@
-# jeff = spawn(fn ->
-#   receive do
-#     {sender, message} -> IO.puts "Received '#{message}' from process #{inspect sender}"
-#   end
-# end)
-#
-# send jeff, {self(), "Hello world"}
